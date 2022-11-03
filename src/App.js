cc3a609a386a15b7e26293c411a3ca4c80b9e924
francisco <franciscolira408@gmail.com>
2022-11-02 20:02:20 -0700

    added code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import "./index.css";
+import React, { Component, useEffect, useState } from 'react';
+import ReactDOM from "react-dom";
+import 'semantic-ui-css/semantic.min.css'
+import { Container, Dropdown, Form, Label, Segment } from 'semantic-ui-react';
+import {Dashboard} from './component/Dashboard/index';
+import {SideBar} from './component/SideBar/index';
+import {Restaurants} from "./Data/Restaurants.json";
+// import "../node_modules/bootstrap/dist/css/bootstrap.css"
+//import Datatable from './lib/component/Datatable';
+import $ from 'jquery';
+
+
+//import React, {useEffect,useState} from 'react'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          yourmom
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div><PersonComp/> </div>
+    <Container style={{height: '100vh'}}>
+     <div><SideBar/></div> 
+     {/* <div><Dashboard/></div>  */}
+    </Container>
+    
+    
   );
 }
 
